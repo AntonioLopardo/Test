c03ac4bcb64398a40df3d78f56895f6e09c4975e
Antonio Lopardo <antonio.lopardo@outlook.com>
2018-04-08 00:52:36 +0200

    Update SearchAlgo.py

--- GitRepo/SearchAlgo.py
+++ GitRepo/SearchAlgo.py
@@ -6,6 +6,16 @@ import datetime as dt
 import load_utilies as lu
 import sys
 
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
+
 def main():
     
     if len(sys.argv) is 1:
